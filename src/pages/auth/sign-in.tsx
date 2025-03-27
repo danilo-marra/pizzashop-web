27f5470e5171ac6343306847d4472f45f2477aca
Danilo Marra Rabelo <danilo2311@gmail.com>
2025-03-27 11:56:56 -0300

    Página de Cadastro

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async';
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router';
 import { toast } from 'sonner';
 import { z } from 'zod';
 
@@ -7,6 +8,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const signInForm = z.object({
   email: z.string().email(),
 });
@@ -45,6 +47,9 @@ export function SignIn() {
     <>
       <Helmet key="signin-helmet" title="Login" />
       <div className="p-8">
+        <Button variant="secondary" asChild className="absolute top-8 right-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">Acessar Painel</h1>
